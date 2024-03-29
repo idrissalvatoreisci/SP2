b03252aeafae402fc24c92a319d78db8bfe1112b
idrissalvatoreisci <158306705+idrissalvatoreisci@users.noreply.github.com>
2024-03-29 15:05:54 +0100

    .

---
+++ tests/FleetOfCarsTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FleetOfCarsTest {
+
+    @Test
+    void addCarsToFleet() {
+    }
+
+    @Test
+    void getTotalCarsInFleet() {
+    }
+
+    @Test
+    void getTotalRegistrationFeeForFleet() {
+    }
+}
